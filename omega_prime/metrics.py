51f8f1ded48eef31ba4fe9eeca8e56564049060f
Michael Schuldes <michael.schuldes@ika.rwth-aachen.de>
2025-05-08 09:09:56 +0200

    add intermediate metric computations

--- omega_prime/metrics.py
+++ omega_prime/metrics.py
@@ -14,13 +14,15 @@ class Metric:
     computes_properties: list[str] = field(default_factory=list)
     requires_columns: list[str] = field(default_factory=list)
     requires_properties: list[str] = field(default_factory=list)
+    computes_intermediate_columns: list[str] = field(default_factory=list)
+    computes_intermediate_properties: list[str] = field(default_factory=list)
     _parameters: list = field(init=False)
 
     def compute_lazy(self, df: pl.LazyFrame, **kwargs) -> tuple[pl.LazyFrame, dict[str, pl.LazyFrame]]:
         try:
             df, properties = self.compute_func(df, **kwargs)
             assert isinstance(df, pl.LazyFrame)
-            assert all(p in properties for p in self.computes_properties)
+            assert all(p in properties for p in self.computes_properties+self.computes_intermediate_properties)
             return df, properties
 
         except TypeError as e:
@@ -54,6 +56,8 @@ def metric(
     computes_properties: list[str] | None = None,
     requires_columns: list[str] | None = None,
     requires_properties: list[str] | None = None,
+    computes_intermediate_columns: list[str] | None = None,
+    computes_intermediate_properties: list[str] | None = None
 ):
     def decorator(func):
         return Metric(
@@ -62,13 +66,15 @@ def metric(
             computes_properties=computes_properties or [],
             requires_columns=requires_columns or [],
             requires_properties=requires_properties or [],
+            computes_intermediate_columns=computes_intermediate_columns or [],
+            computes_intermediate_properties=computes_intermediate_properties or [],
         )
 
     return decorator
 
 
-@metric(computes_columns=["distance_traveled", "vel"])
-def driven_distance_and_vel(df) -> tuple[pl.DataFrame, dict[str, pl.DataFrame]]:
+@metric(computes_columns=["distance_traveled"])
+def distance_traveled(df) -> tuple[pl.DataFrame, dict[str, pl.DataFrame]]:
     return df.with_columns(
         (pl.col("x").diff() ** 2 + pl.col("y").diff() ** 2)
         .sqrt()
@@ -76,15 +82,22 @@ def driven_distance_and_vel(df) -> tuple[pl.DataFrame, dict[str, pl.DataFrame]]:
         .fill_null(0.0)
         .cum_sum()
         .alias("distance_traveled"),
+        
+    ), {}
+
+@metric(computes_columns=['vel'])
+def vel(df) -> tuple[pl.DataFrame, dict[str, pl.DataFrame]]:
+    return df.with_columns(
         (pl.col("vel_x") ** 2 + pl.col("vel_y") ** 2).sqrt().alias("vel"),
     ), {}
 
 
 @metric(
     requires_columns=["distance_traveled", "vel"],
-    computes_properties=["timegaps", "min_timegaps", "p_timegaps", "min_p_timegaps"],
+    computes_properties=["timegaps", "min_timegaps"],
+    computes_intermediate_properties=['crossed']
 )
-def timegaps_and_p_timegaps(df, /, ego_id, time_buffer=2e9):
+def timegaps_and_min_timgaps(df, /, ego_id, time_buffer=2e9):
     ego_df = df.filter(idx=ego_id)
 
     crossed = df.join(ego_df, how="cross", suffix="_ego")
@@ -119,6 +132,19 @@ def timegaps_and_p_timegaps(df, /, ego_id, time_buffer=2e9):
         pl.col("timegap").get(pl.col("timegap").abs().arg_min()).alias("min_timegap")
     )
 
+    return df, {
+        "timegaps": timegaps,
+        "min_timegaps": min_timegaps,
+        "crossed": crossed
+    }
+    
+
+@metric(
+    requires_columns=["distance_traveled", "vel"],
+    requires_properties=['crossed','timegaps'],
+    computes_properties=["p_timegaps", "min_p_timegaps"],
+)
+def p_timegaps_and_min_p_timgaps(df, /, ego_id, crossed, timegaps,time_buffer=2e9):
     p_timegaps = (
         crossed.join(timegaps, how="right", suffix="_overlap", on=["idx", "idx_ego"])
         .with_columns(
@@ -152,14 +178,17 @@ def timegaps_and_p_timegaps(df, /, ego_id, time_buffer=2e9):
     )
 
     return df, {
-        "timegaps": timegaps,
-        "min_timegaps": min_timegaps,
         "p_timegaps": p_timegaps,
         "min_p_timegaps": min_p_timegaps,
     }
 
 
-metrics = [timegaps_and_p_timegaps, driven_distance_and_vel]
+metrics = [
+    vel,
+    distance_traveled,
+    timegaps_and_min_timgaps, 
+    p_timegaps_and_min_p_timgaps
+]
 
 
 @dataclass
@@ -175,7 +204,7 @@ class MetricManager:
         self._dependencies = {
             val: [i]
             for i, m in enumerate(self.metrics)
-            for val in [f"column_{n}" for n in m.computes_columns] + [f"property_{n}" for n in m.computes_properties]
+            for val in [f"column_{n}" for n in m.computes_columns+m.computes_intermediate_columns] + [f"property_{n}" for n in m.computes_properties+m.computes_intermediate_properties]
         } | {
             i: [f"column_{n}" for n in m.requires_columns] + [f"property_{n}" for n in m.requires_properties]
             for i, m in enumerate(self.metrics)
@@ -191,6 +220,9 @@ class MetricManager:
             )
 
         self._parameters = [v for m in self.metrics for v in m._parameters]
+
+        self.exclude_columns += [v for m in self.metrics for v in m.computes_intermediate_columns]
+        self.exclude_properties += [v for m in self.metrics for v in m.computes_intermediate_properties]
 
         ts = graphlib.TopologicalSorter(self._dependencies)
         self._ordered_metrics = [self.metrics[o] for o in ts.static_order() if isinstance(o, int)]
