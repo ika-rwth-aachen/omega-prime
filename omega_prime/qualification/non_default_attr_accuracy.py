1e68ff6ea4daba1b55e7cd4ce821d70ea4115049
Peter Koval <pkoval@vicomtech.org>
2026-02-11 12:14:38 +0100

    +2 docstrings

--- omega_prime/qualification/non_default_attr_accuracy.py
+++ omega_prime/qualification/non_default_attr_accuracy.py
@@ -12,12 +12,34 @@ NON_DEFAULT_ATTRIBUTES_ACCURACY = "non_default_attributes_accuracy"
 
 
 def get_column_names(t: type[int] | type[float]) -> list[str]:
+    """List of column names of a given type.
+
+    Args:
+        t: should be `int` or `float`.
+
+    Returns:
+        list of column names selected from the schema.
+    """
     pl_type = pl.Int64 if t is int else pl.Float64
     return [k for k, v in polars_schema.items() if v == pl_type]
 
 
 @metric(computes_properties=[NON_DEFAULT_ATTRIBUTES_ACCURACY])
 def non_default_attributes_accuracy(df: pl.LazyFrame | pl.DataFrame, /, columns: Sequence[str] = tuple()) -> QRT:
+    """Non default valued attributes accuracy.
+
+        Measures how often attributes contain non-default values, reflecting data richness.
+        Flags overused defaults (e.g., unknown).
+        Formula is (number of non-default value) * 100 / (total number of values)
+        Threshold is greater or equal than 95%.
+
+    Args:
+        df: omega-prime data frame (LazyFrame).
+        columns: columns to query. If not given, the schema keys are used.
+
+    Returns:
+        The tuple of original data and qualification summary (dictionary).
+    """
     cc = columns if columns else polars_schema.keys()
     s_cc = df.select(cc)
     select = s_cc.select(pl.sum_horizontal(*[pl.col(c) == 0 for c in cc]).sum())
