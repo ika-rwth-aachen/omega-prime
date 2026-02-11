c60bb9256111f3b360a694da98abd27cc665a260
Peter Koval <pkoval@vicomtech.org>
2026-02-11 10:30:33 +0100

    reformat my things

--- tests/qualification/test_non_default_attr_accuracy.py
+++ tests/qualification/test_non_default_attr_accuracy.py
@@ -2,7 +2,8 @@
 
 from omega_prime import Recording
 from omega_prime.qualification.non_default_attr_accuracy import (
-    non_default_attributes_accuracy, NON_DEFAULT_ATTRIBUTES_ACCURACY
+    non_default_attributes_accuracy,
+    NON_DEFAULT_ATTRIBUTES_ACCURACY,
 )
 
 from .conftest import qualification_assert
@@ -11,4 +12,3 @@ from .conftest import qualification_assert
 def test_pass(rec: Recording) -> None:
     _df, result_dict = non_default_attributes_accuracy(rec.df)
     qualification_assert(result_dict, NON_DEFAULT_ATTRIBUTES_ACCURACY, 100.0, True)
-
