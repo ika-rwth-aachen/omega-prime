d79c3980fc1f722314eda1c99d71bf4497c66086
Peter Koval <pkoval@vicomtech.org>
2026-02-20 19:43:53 +0100

    +raise value error

--- omega_prime/qualification/non_default_attr_accuracy.py
+++ omega_prime/qualification/non_default_attr_accuracy.py
@@ -28,7 +28,8 @@ def get_column_names(t: type[int] | type[float]) -> list[str]:
 POLARS_T = pl.Int64 | pl.Float64 | pl.Float32 | pl.Binary | pl.Object | pl.String
 
 
-def get_default_value(polars_type: POLARS_T) -> float | int | str | bytes | None:
+def get_default_value(polars_type: POLARS_T) -> float | int | str | bytes:
+    x = 0  # silencing the code checker in PyCharm.
     # fmt: off
     match polars_type:
         case pl.Int64: x = 0
@@ -42,7 +43,7 @@ def get_default_value(polars_type: POLARS_T) -> float | int | str | bytes | None
 
 
 @metric(computes_properties=[NON_DEFAULT_ATTRIBUTES_ACCURACY])
-def non_default_attributes_accuracy(df: pl.LazyFrame | pl.DataFrame, /, columns: Sequence[str] = tuple()) -> QRT:
+def non_default_attributes_accuracy(df: pl.LazyFrame, /, columns: Sequence[str] = tuple()) -> QRT:
     """Non default valued attributes accuracy.
 
         Measures how often attributes contain non-default values, reflecting data richness.
@@ -50,13 +51,10 @@ def non_default_attributes_accuracy(df: pl.LazyFrame | pl.DataFrame, /, columns:
         Formula is (number of non-default value) * 100 / (total number of values)
         Threshold is greater or equal than 95%.
 
-        Design choices:
-          - the keyword argument `columns` allows to analyze a custom set of columns, even those
-        outside the OmegaPrime data schema `polars_schema`. The default values for the columns
-        outside the OmegaPrime data schema are derived from the column types of the data frame.
-
-          - if some columns are missing in the data frame, then a message is printed,
-        **but no exception is raised**.
+        Design choices: the keyword argument `columns` allows to analyze a custom set of columns,
+        even those outside the OmegaPrime data schema `polars_schema`.
+        The default values for the columns outside the OmegaPrime data schema are derived
+        from the schema of the data frame.
 
     Args:
         df: omega-prime data frame (LazyFrame).
@@ -64,6 +62,10 @@ def non_default_attributes_accuracy(df: pl.LazyFrame | pl.DataFrame, /, columns:
 
     Returns:
         The tuple of original data and qualification summary (dictionary).
+
+    Raises:
+        ValueError: if some columns could not be found.
+
     """
     schema = df.collect_schema()
     cc = set(columns) if columns else polars_schema.keys()
@@ -71,7 +73,7 @@ def non_default_attributes_accuracy(df: pl.LazyFrame | pl.DataFrame, /, columns:
     if absent_cols:
         is_verb, ending = get_is_ending(len(absent_cols))
         items = format_items(sorted(absent_cols))
-        print(f"Column{ending} {items} {is_verb} absent in the data frame.")
+        raise ValueError(f"Column{ending} {items} {is_verb} absent in the data frame.")
 
     existing_cols = cc - absent_cols
     s_cc = df.select(existing_cols)
--- tests/qualification/non_default_attr_accuracy/test_custom_columns_missing.py
+++ tests/qualification/non_default_attr_accuracy/test_custom_columns_missing.py
@@ -1,34 +1,25 @@
-import polars as pl
 import pytest
 
 from omega_prime import Recording
 from omega_prime.qualification.non_default_attr_accuracy import non_default_attributes_accuracy
 
-from .conftest import this_assert
+
+def test_x_is_missing(rec: Recording) -> None:
+    manipulated_df1 = rec.df.drop("x")
+    with pytest.raises(ValueError) as err:
+        non_default_attributes_accuracy(manipulated_df1)
+    assert err.value.args[0] == "Column x is absent in the data frame."
 
 
-def test_x_is_missing(rec: Recording, capsys: pytest.CaptureFixture) -> None:
-    df: pl.DataFrame = rec.df
-    manipulated_df = df.drop("x")
-    _df, result_dict = non_default_attributes_accuracy(manipulated_df)
-    this_assert(result_dict, 65.55299539170507, False)
-    stdout = capsys.readouterr().out
-    assert stdout == "Column x is absent in the data frame.\n"
+def test_x_and_y_are_missing(rec: Recording) -> None:
+    manipulated_df = rec.df.drop("x", "y")
+    with pytest.raises(ValueError) as err:
+        non_default_attributes_accuracy(manipulated_df)
+    assert err.value.args[0] == "Columns x and y are absent in the data frame."
 
 
-def test_x_and_y_are_missing(rec: Recording, capsys: pytest.CaptureFixture) -> None:
-    df: pl.DataFrame = rec.df
-    manipulated_df = df.drop("x", "y")
-    _df, result_dict = non_default_attributes_accuracy(manipulated_df)
-    this_assert(result_dict, 63.63927291346646, False)
-    stdout = capsys.readouterr().out
-    assert stdout == "Columns x and y are absent in the data frame.\n"
-
-
-def test_x_y_and_z_are_missing(rec: Recording, capsys: pytest.CaptureFixture) -> None:
-    df: pl.DataFrame = rec.df
-    manipulated_df = df.drop("x", "y", "z")
-    _df, result_dict = non_default_attributes_accuracy(manipulated_df)
-    this_assert(result_dict, 61.50040661425861, False)
-    stdout = capsys.readouterr().out
-    assert stdout == "Columns x, y and z are absent in the data frame.\n"
+def test_x_y_and_z_are_missing(rec: Recording) -> None:
+    manipulated_df = rec.df.drop("x", "y", "z")
+    with pytest.raises(ValueError) as err:
+        non_default_attributes_accuracy(manipulated_df)
+    assert err.value.args[0] == "Columns x, y and z are absent in the data frame."
