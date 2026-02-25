f5713b17ca2a000a1cb4374eb1ba7ca9716b8e67
Guido Linden <guido.linden@ika.rwth-aachen.de>
2026-02-25 10:19:42 +0100

    use lazy frames exclusively

--- omega_prime/qualification/temporal_completeness.py
+++ omega_prime/qualification/temporal_completeness.py
@@ -3,37 +3,31 @@
 import polars as pl
 
 from ..metrics import metric
-from .common import STATUS, PASS, FAIL, QRT
+from .common import STATUS, PASS, FAIL, QRT, get_num_rows
 
 TEMPORAL_COMPLETENESS = "temporal_completeness"
 
 
 @metric(computes_properties=[TEMPORAL_COMPLETENESS])
 def temporal_completeness(
-    df: pl.LazyFrame | pl.DataFrame,
+    df: pl.LazyFrame,
     /,
     expected_frequency: float,
 ) -> QRT:
-    DELTA_TARGET = 1.0 / expected_frequency
-    TRACK_THRESHOLD = 0.95
+    delta_target = 1.0 / expected_frequency
+    track_threshold = 0.95
 
     idx_and_time = df.select("idx", "total_nanos").sort(["idx", "total_nanos"])
     deltas = idx_and_time.with_columns((pl.col("total_nanos").diff().over("idx") / 1e9).alias("delta_t"))
     valid_deltas = deltas.filter(pl.col("delta_t").is_not_null())
     per_track_rms = valid_deltas.group_by("idx").agg(
-        (((pl.col("delta_t") - DELTA_TARGET) / DELTA_TARGET).pow(2).mean().sqrt()).alias("delta_rms")
+        (((pl.col("delta_t") - delta_target) / delta_target).pow(2).mean().sqrt()).alias("delta_rms")
     )
-    below_count_query = per_track_rms.select((pl.col("delta_rms") > (1 - TRACK_THRESHOLD)).sum().alias("below_count"))
+    below_count_query = per_track_rms.select((pl.col("delta_rms") > (1 - track_threshold)).sum().alias("below_count"))
 
-    if isinstance(below_count_query, pl.LazyFrame):
-        below_count = int(below_count_query.collect().item(0, 0))
-    else:
-        below_count = int(below_count_query.item(0, 0))
+    below_count = int(below_count_query.collect().item(0, 0))
 
-    if isinstance(per_track_rms, pl.LazyFrame):
-        total_tracks = per_track_rms.select(pl.len()).collect().item(0, 0)
-    else:
-        total_tracks = per_track_rms.shape[0]
+    total_tracks = get_num_rows(per_track_rms)
         
     if total_tracks > 0:
         temporal_completeness = (1.0 - (below_count / total_tracks)) * 100.0
