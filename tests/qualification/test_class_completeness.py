0652b2af723dbfa8fe24535e4aa6789698c7948d
Guido Linden <guido.linden@ika.rwth-aachen.de>
2026-02-25 11:33:49 +0100

    add test againts pedestrian.osi

--- tests/qualification/test_class_completeness.py
+++ tests/qualification/test_class_completeness.py
@@ -1,12 +1,24 @@
 """."""
 
 import betterosi
+from omega_prime.recording import Recording
 import pytest
 import polars as pl
 
 from omega_prime.qualification.class_completeness import class_completeness, CLASS_COMPLETENESS
 
 from .conftest import qualification_assert
+
+expected_pass = [
+    betterosi.MovingObjectType.TYPE_PEDESTRIAN,
+    betterosi.MovingObjectType.TYPE_VEHICLE,
+]
+
+expected_fail = [
+    betterosi.MovingObjectType.TYPE_PEDESTRIAN,
+    betterosi.MovingObjectType.TYPE_VEHICLE,
+    betterosi.MovingObjectType.TYPE_ANIMAL,
+]
 
 @pytest.fixture()
 def class_df() -> pl.LazyFrame:
@@ -21,19 +33,18 @@ def class_df() -> pl.LazyFrame:
     ).lazy()
 
 def test_pass(class_df) -> None:
-    expected = [
-        betterosi.MovingObjectType.TYPE_PEDESTRIAN,
-        betterosi.MovingObjectType.TYPE_VEHICLE,
-    ]
-    _df, result_dict = class_completeness(class_df, expected_classes=expected)
+    _df, result_dict = class_completeness(class_df, expected_classes=expected_pass)
     qualification_assert(result_dict, CLASS_COMPLETENESS, 100.0, True)
 
-
 def test_fail(class_df) -> None:
-    expected = [
-        betterosi.MovingObjectType.TYPE_PEDESTRIAN,
-        betterosi.MovingObjectType.TYPE_VEHICLE,
-        betterosi.MovingObjectType.TYPE_ANIMAL,
-    ]
-    _df, result_dict = class_completeness(class_df, expected_classes=expected)
+    expected = expected_fail
+    _df, result_dict = class_completeness(class_df, expected_classes=expected_fail)
+    qualification_assert(result_dict, CLASS_COMPLETENESS, 66.66666666666667, False)
+
+def test_record_pass(rec: Recording) -> None:
+    df, result_dict = class_completeness(rec.df.lazy(), expected_classes=expected_pass)
+    qualification_assert(result_dict, CLASS_COMPLETENESS, 100.0, True)
+
+def test_record_fail(rec: Recording) -> None:
+    df, result_dict = class_completeness(rec.df.lazy(), expected_classes=expected_fail)
     qualification_assert(result_dict, CLASS_COMPLETENESS, 66.66666666666667, False)
