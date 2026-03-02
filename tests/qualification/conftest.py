38f6af5182527992999c44c894e3af82443ef6d2
Guido Linden <guido.linden@ika.rwth-aachen.de>
2026-03-02 13:15:24 +0100

    revert qualification_assert

--- tests/qualification/conftest.py
+++ tests/qualification/conftest.py
@@ -14,16 +14,7 @@ def rec(files_dir: Path):
     return omega_prime.Recording.from_file(str(files_dir / "pedestrian.osi"))
 
 
-def qualification_assert(
-    qd: dict[str, pl.LazyFrame],
-    metric_name: str,
-    expected_value: float,
-    is_pass: bool,
-    sub_metric_name: str | None = None,
-) -> None:
+def qualification_assert(qd: dict[str, pl.LazyFrame], metric_name: str, expected_value: float, is_pass: bool) -> None:
     result_df = qd[metric_name].collect()
-    if sub_metric_name is not None:
-        assert result_df[sub_metric_name][0] == pytest.approx(expected_value)
-    else:
-        assert result_df[metric_name][0] == pytest.approx(expected_value)
+    assert result_df[metric_name][0] == pytest.approx(expected_value)
     assert result_df[STATUS][0] == PASS if is_pass else FAIL
