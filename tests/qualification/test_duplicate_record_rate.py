e16cc3ff18d0cc44b0999ad0e8f66bc10a819511
Guido Linden <guido.linden@ika.rwth-aachen.de>
2026-03-04 10:01:52 +0100

    alignment to other tests

--- tests/qualification/test_duplicate_record_rate.py
+++ tests/qualification/test_duplicate_record_rate.py
@@ -3,44 +3,48 @@
 import pytest
 import polars as pl
 
-from omega_prime.qualification.duplicate_record_rate import duplicate_record_rate
+from omega_prime.qualification.duplicate_record_rate import (
+    duplicate_record_rate,
+    DUPLICATE_RECORD_RATE,
+    DUPLICATE_RECORD_DUPLICATES,
+)
+
+from .conftest import qualification_assert
 
 
 @pytest.fixture()
-def duplicate_df_pass() -> pl.DataFrame:
+def duplicate_df_pass() -> pl.LazyFrame:
     return pl.DataFrame(
         {
             "idx": [1, 1, 2, 2],
             "total_nanos": [0, 1, 0, 1],
         }
-    )
+    ).lazy()
 
 
 @pytest.fixture()
-def duplicate_df_fail() -> pl.DataFrame:
+def duplicate_df_fail() -> pl.LazyFrame:
     return pl.DataFrame(
         {
             "idx": [1, 1, 1, 2, 2, 2],
             "total_nanos": [0, 0, 0, 1, 1, 1],
         }
-    )
+    ).lazy()
 
 
 def test_pass(duplicate_df_pass) -> None:
     _df, result_dict = duplicate_record_rate(duplicate_df_pass, threshold=1.0)
-    summary = result_dict["duplicate_record_rate"].collect()
+    summary = result_dict[DUPLICATE_RECORD_RATE].collect()
     assert summary["total_records"][0] == 4
     assert summary["duplicate_records"][0] == 0
-    assert summary["duplicate_record_rate"][0] == pytest.approx(0.0)
-    assert summary["status"][0] == "pass"
+    qualification_assert(result_dict, DUPLICATE_RECORD_RATE, 0.0, True)
 
 
 def test_fail(duplicate_df_fail) -> None:
     _df, result_dict = duplicate_record_rate(duplicate_df_fail, threshold=1.0)
-    summary = result_dict["duplicate_record_rate"].collect()
-    duplicates = result_dict["duplicate_record_duplicates"].collect()
+    summary = result_dict[DUPLICATE_RECORD_RATE].collect()
+    duplicates = result_dict[DUPLICATE_RECORD_DUPLICATES].collect()
     assert summary["total_records"][0] == 6
     assert summary["duplicate_records"][0] == 4
-    assert summary["duplicate_record_rate"][0] == pytest.approx(66.66666666666667)
-    assert summary["status"][0] == "fail"
+    qualification_assert(result_dict, DUPLICATE_RECORD_RATE, 66.66666666666667, False)
     assert duplicates["duplicate_records"].sum() == 4
