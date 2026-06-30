7d908cf3d28c9a1fe6cbf2665eda9ce03b17d7dc
chrysanthidis <stamatios.chrysanthidis@ika.rwth-aachen.de>
2026-06-30 10:28:27 +0000

    fix(polars): add 2D row indexing fallback for older Polars versions

--- omega_prime/locator.py
+++ omega_prime/locator.py
@@ -232,7 +232,12 @@ class Locator:
         l_ids = np.array([self.external2internal_laneid[i] for i in sts["roadlane_id"].to_numpy()])
         for l_id in set(l_ids):
             point_idxs = np.argwhere(l_ids == l_id)[:, 0]
-            rel_sts = sts.gather(point_idxs) if hasattr(sts, "gather") else sts.take(point_idxs)
+            if hasattr(sts, "gather"):
+                rel_sts = sts.gather(point_idxs)
+            elif hasattr(sts, "take"):
+                rel_sts = sts.take(point_idxs)
+            else:
+                rel_sts = sts[point_idxs, :]
             l = self.extended_centerlines[l_id]
             xys[point_idxs, 0], xys[point_idxs, 1] = ShapelyTrajectoryTools.st2xy(
                 l, rel_sts["s"].to_numpy() + ShapelyTrajectoryTools.l_append, rel_sts["t"].to_numpy()
